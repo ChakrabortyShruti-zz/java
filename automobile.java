88eb7848952e35291f787ceef26a402f47d166df
Shruti Chakraborty <shruti.chakraborty16@gmail.com>
2016-02-16 14:55:44 +0530

    Made constructor as private and also made functions for printing the values.

--- automobile.java
+++ automobile.java
@@ -3,21 +3,29 @@ class Automobile{
     private int year;
     private int cost;
 
-    public Automobile(String model,int yearOfRelease){
+    private Automobile(String model,int yearOfRelease){
         this.name = model;
         this.year = yearOfRelease;
     }
 
-    public Automobile(int price,String name){
+    public static Automobile getInfo(String title,int years){
+        return new Automobile(title,years);
+    }
+
+    private Automobile(int price,String name){
         this.cost = price;
         this.name = name;
+    }
+
+    public static Automobile getInfo(int price,String name){
+        return new Automobile(price,name);
     }
 
     public void printCarNameWithYear(){
         System.out.println("The name of the car is "+this.name+" and is released on "+this.year);
     }
 
-    public  void printCarNameWithPrice(){
+    public void printCarNameWithPrice(){
         System.out.println("The name of the car is "+this.name+" and price is "+this.cost);
     }
 }
--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
-class hello{
+class Hello{
     public static void main(String[] args){
-        Automobile car = new Automobile("XUV",2014);
-        Automobile costliestCar = new Automobile(10000000,"BMW");
+        Automobile car  = Automobile.getInfo("XUV",2014);
+        Automobile costliestCar = Automobile.getInfo(100000000,"BMW");
         car.printCarNameWithYear();
         costliestCar.printCarNameWithPrice();
     }
